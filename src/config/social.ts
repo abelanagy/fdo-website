36796e134aa1f01fe6c7b4e93f30ddd78f42f560
Nimród Simon <snimrod28@gmail.com>
2025-05-08 00:06:25 +0200

    Add social media links and icons

---
+++ src/config/social.ts
@@ -0,0 +1,33 @@
+import FacebookIcon from '@/images/logos/facebook.svg'
+import InstagramIcon from '@/images/logos/instagram.svg'
+import TikTokIcon from '@/images/logos/tiktok.svg'
+import GoogleDriveIcon from '@/images/logos/drive.svg'
+import LinkTreeIcon from '@/images/logos/linktree.svg'
+
+export const socialLinks = [
+  {
+    name: 'LinkTree',
+    href: 'https://linktr.ee/fovarosidok',
+    icon: LinkTreeIcon,
+  },
+  {
+    name: 'Facebook csoport',
+    href: 'https://www.facebook.com/groups/1035812251522846',
+    icon: FacebookIcon,
+  },
+  {
+    name: 'Instagram',
+    href: 'https://www.instagram.com/fovarosidok',
+    icon: InstagramIcon,
+  },
+  {
+    name: 'TikTok',
+    href: 'https://www.tiktok.com/@fovarosidok',
+    icon: TikTokIcon,
+  },
+  {
+    name: 'Alapdokumentumok',
+    href: 'https://drive.google.com/drive/u/2/folders/1X31jjpNhfY2LLPLvWUBDUL-RX96-svXj?fbclid=PAZXh0bgNhZW0CMTEAAaeiFXHqM0nu_pAiJqNRKdYCYds9scjl9uEPvo1B3SJmaBQpOFaBesf6d7xY-w_aem_7Fvq0VAvmyptuiqrFmWucg',
+    icon: GoogleDriveIcon,
+  },
+]
