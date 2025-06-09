294ef4e005b85dfa09ef4202030c24f0b771eda0
abeln <abel.nagy2601@gmail.com>
2025-06-09 22:15:57 +0200

    feat: update homepage content to reflect new organizational focus and language

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -92,7 +92,7 @@ function Article({ article }: { article: ArticleWithSlug }) {
         {formatDate(article.date)}
       </Card.Eyebrow>
       <Card.Description>{article.description}</Card.Description>
-      <Card.Cta>Read article</Card.Cta>
+      <Card.Cta>Részletek</Card.Cta>
     </Card>
   )
 }
@@ -273,13 +273,15 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Software designer, founder, and amateur astronaut.
+            Fővárosi Diákönkormányzat
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            A Fővárosi Diákönkormányzat Budapest csaknem 300 köznevelési
+            intézményének (gimnáziumok, technikumok, szakképző iskolák)
+            diák-érdekképviseleti ernyőszervezete. Célja, hogy a kb. 200 000
+            fővárosi középiskolás közös hangjaként jelenjen meg a városháza, a
+            kormányzati szereplők és a civil szféra előtt, s fórumot biztosítson
+            a diákok javaslatainak, kritikáinak és projektjeinek.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink href="#" aria-label="Follow on X" icon={XIcon} />
