6bc0737948e9caa588e40e59d8e74c6e57bb30f1
Nimród Simon <snimrod28@gmail.com>
2025-06-12 00:33:51 +0200

    feat: enhance Csatlakozz page with step-by-step guide and animations

--- src/app/(root)/csatlakozz/page.tsx
+++ src/app/(root)/csatlakozz/page.tsx
@@ -1,56 +1,108 @@
 import { type Metadata } from 'next'
 
 import { SimpleLayout } from '@/components/SimpleLayout'
+import * as motion from 'motion/react-client'
 
 export const metadata: Metadata = {
   title: 'Csatlakozz!',
-  description: '',
-} // TODO: add description
+  description: 'Útmutató a Diákparlamenthez való csatlakozáshoz.',
+}
 
-export default function Csatlakozz() {
+type StepType = {
+  order: number
+  title: string
+  description: string | React.ReactNode
+}
+
+const steps: StepType[] = [
+  {
+    order: 1,
+    title: 'Delegálólap kitöltése',
+    description: (
+      <span>
+        Ahhoz hogy diákparlamentjeinkre járj, ahhoz minden egyes alkalomkor kell
+        szerezned egy delegálólapot. A delegálólapot az intézményvezetőknek
+        szoktuk kiküldeni, de mostantól itt is kitölthető{' '}
+        <a
+          href="https://forms.fillout.com/t/7SAZC9A3xLus"
+          className="bg-red-50 text-red-500 hover:underline dark:bg-red-900/20 dark:text-red-400"
+        >
+          az alábbi oldalon ➜
+        </a>
+      </span>
+    ),
+  },
+  {
+    order: 2,
+    title: 'Aláírások beszerzése',
+    description:
+      'A kinyomtatott delegálólapot a Diákönkormányzat elnöknek és az Igazgatónak alá kell írnia.',
+  },
+  {
+    order: 3,
+    title: 'Email küldése',
+    description: (
+      <span>
+        A lapot beszkennelve, 24 órával a diákparlament kezdete előtt emailben
+        küldd el a{' '}
+        <a
+          href="mailto:fovarosidiakonkormanyzat@gmail.com"
+          className="text-red-500 hover:underline"
+        >
+          fovarosidiakonkormanyzat@gmail.com
+        </a>{' '}
+        címre.
+      </span>
+    ),
+  },
+  {
+    order: 4,
+    title: 'Kapcsolatfelvétel a bizottságokhoz',
+    description:
+      'Ha a kormány, etikai bizottság vagy a mentálhigiénés bizottság része lenni, akkor vedd fel a kapcsolatot velünk a fent kiemelt email címen és a jelenlegi helyzet alapján minden további információt elküldünk neked ezzel kapcsolatban.',
+  },
+]
+
+function StepCard({ step }: Readonly<{ step: StepType }>) {
+  return (
+    <motion.div
+      className="md:items-baseline"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 1 }}
+    >
+      <div className="flex gap-4 max-md:flex-col max-sm:mx-4 md:items-center">
+        <div className="m-0 flex h-12 min-w-12 items-center justify-center rounded-full bg-red-500 pb-0.5 text-4xl leading-none font-semibold text-white max-md:max-w-12 md:h-8 md:min-w-8 md:text-2xl">
+          {step.order}
+        </div>
+        <div>
+          <h3 className="text-lg font-semibold text-zinc-800 dark:text-zinc-200">
+            {step.title}
+          </h3>
+          <p className="mt-1 text-sm text-zinc-600 dark:text-zinc-400">
+            {step.description}
+          </p>
+        </div>
+      </div>
+    </motion.div>
+  )
+}
+
+export default async function Csatlakozz() {
   return (
     <SimpleLayout
       title="Csatlakozz!"
-      intro="One of my favorite ways to share my ideas is live on stage, where there’s so much more communication bandwidth than there is in writing, and I love podcast interviews because they give me the opportunity to answer questions instead of just present my opinions."
+      intro="Szeretnél részt venni a Diákparlament gyűlésein?
+      Csak négy egyszerű lépés szükséges hozzá!"
     >
-      <form>
-        <div className="mb-4">
-          <label
-            htmlFor="name"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Név
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            required
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
-          />
+      <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+        <div className="grid max-w-3xl grid-cols-1 gap-16 sm:grid-cols-2 md:grid-cols-1 md:gap-8">
+          {steps.map((step) => (
+            <StepCard key={step.title} step={step} />
+          ))}
         </div>
-        <div className="mb-4">
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            required
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
-          />
-        </div>
-        <button
-          type="submit"
-          className="rounded-md bg-blue-500 px-4 py-2 text-white"
-        >
-          Csatlakozz!
-        </button>
-      </form>
+      </div>
     </SimpleLayout>
   )
 }
